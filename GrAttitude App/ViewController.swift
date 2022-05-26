5c9d2e588068efd60c592dbaaebe6af06e0b1a3e
Jackie Wong <jcwong91@gmail.com>
2022-05-26 11:42:24 -0400

    Initial Commit

---
+++ GrAttitude App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GrAttitude App
+//
+//  Created by Wong, Jacqueline on 5/26/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
